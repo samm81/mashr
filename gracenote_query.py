0935e34c804fe9049fa77dd233eb7774b1c74746
JacobVanGeffen <jsvangeffen@gmail.com>
2015-03-18 22:56:16 -0500

    getBeats

--- gracenote_query.py
+++ gracenote_query.py
@@ -2,14 +2,14 @@ import json
 import requests
 
 def getSegments(filename):
-	url2 = 'http://devapi.gracenote.com/timeline/api/1.0/audio/extract/'
+	# url2 = 'http://devapi.gracenote.com/timeline/api/1.0/audio/extract/'
 	url = 'http://odp-server-env-xpubununej.elasticbeanstalk.com/api/1.0/audio/extract/'
 
 	req = requests.post(url, files={"audio_file": open(filename, 'rb')})
 
 	url = 'http://devapi.gracenote.com/timeline/api/1.0/audio/features/'
 
-	print req.text
+	# print req.text
 	while True:
 		try:
 			audio_id = req.json()['audio_id'] 
@@ -24,7 +24,7 @@ def getSegments(filename):
 	features = json.loads(data['features'])
 	# print features
 	# print the bpm
-	print features['timeline']['segment']
+	# print features['timeline']['segment']
 
 	# make the array of tuples
 	list = []
@@ -32,3 +32,30 @@ def getSegments(filename):
 		list.append((item['start'], item['end'], item['label']))
 
 	return list
+
+def getBeats(filename):
+        # url2 = 'http://devapi.gracenote.com/timeline/api/1.0/audio/extract/'
+        url = 'http://odp-server-env-xpubununej.elasticbeanstalk.com/api/1.0/audio/extract/'
+
+        req = requests.post(url, files={"audio_file": open(filename, 'rb')})
+
+        url = 'http://devapi.gracenote.com/timeline/api/1.0/audio/features/'
+
+        # print req.text
+        while True:
+                try:
+                        audio_id = req.json()['audio_id']
+                        break
+                except KeyError:
+                        continue
+        req = requests.get(url + audio_id)
+        data = req.json()
+        while data['job_status']!='1':
+                req = requests.get(url + audio_id)
+                data = req.json()
+        features = json.loads(data['features'])
+        # print features
+        # print the bpm
+        # print features['timeline']['beat']
+
+        return (features['meta']['bpm'], features['meta']['duration'])
