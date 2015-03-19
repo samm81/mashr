362331ea502230183ef5ae226bfe4a44a006585d
sam <samwmaynard@gmail.com>
2015-03-19 09:01:00 -0500

    webserver working

--- mixup.py
+++ mixup.py
@@ -36,40 +36,29 @@ def getSong(path):
 def export (song, name):
 	song.export(name, format="mp3")
 
+def mix_from_paths(path1, path2):
+	print 'loaded and alive'
+	print 'song 1 is', path1
+	print 'song 2 is', path2
 
+	print 'getting beats for song 1...'
+	features1 = getFeatures(path1)
+	beats1 = getBeats(features1)
+	print 'recieved beats for song 1'
 
-#path1 = "Blank Space.mp3"
-#path2 = "Style.mp3"
-#segments1 = [5, 11]
-#segments2 = [20, 26]
-#mixup(path1, path2, segments1, segments2)
-#path1 = "alpha.mp3"
-#path2 = "jump.mp3"
-path1 = sys.argv[1]
-path2 = sys.argv[2]
-print 'loaded and alive'
-print 'song 1 is', path1
-print 'song 2 is', path2
+	print 'getting beats for song 2'
+	features2 = getFeatures(path2)
+	beats2 = getBeats(features2)
+	print 'recieved beats for song 2'
 
-print 'getting beats for song 1...'
-features1 = getFeatures(path1)
-beats1 = getBeats(features1)
-print 'recieved beats for song 1'
+	print 'creating the mashup!'
+	introTime = getIntro(features1)
+	print 'introTime:', introTime
+	outroTime = getOutro(features2)
+	print 'outroTime:', outroTime
 
-print 'getting beats for song 2'
-#print 'segments1:', segments1
-features2 = getFeatures(path2)
-beats2 = getBeats(features2)
-#print 'segments2:', segments2
-#segments1 = [(0,30),(30,45),(45,60)]
-#segments2 = [(0,15),(15,30),(30,60)]
-print 'recieved beats for song 2'
+	export(mixup(path1, path2, beats1, beats2, introTime, outroTime), "static/thisisntevenmyfinalform.mp3")
+	print 'mashup created - all done'
 
-print 'creating the mashup!'
-introTime = getIntro(features1)
-print 'introTime:', introTime
-outroTime = getOutro(features2)
-print 'outroTime:', outroTime
-
-export(mixup(path1, path2, beats1, beats2, introTime, outroTime), "thisisntevenmyfinalform.mp3")
-print 'mashup created - all done'
+#path1 = sys.argv[1]
+#path2 = sys.argv[2]
