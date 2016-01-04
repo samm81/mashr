6020b3b9d6703142703f9042bb38d06da4db96fb
samm81 <samwmaynard@gmail.com>
2016-01-04 16:46:12 -0500

    better logging

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from music_graph import getVerbal, getFeedback
 from mp3_metadata import getSongInfo
 from threading import Thread
 import uuid
+import logging
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,54 +14,66 @@ socketio = SocketIO(app)
 # Disable caching to fix refresh bug
 @app.after_request
 def add_header(response):
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+	response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+	response.headers['Cache-Control'] = 'public, max-age=0'
+	return response
 
 @app.route("/")
 def hello():
-    id = str(uuid.uuid4())
-    resp = make_response(render_template('index.html'))
-    resp.set_cookie('id', id)
-    print 'id:', id
-    return resp
+	id = str(uuid.uuid4())
+	resp = make_response(render_template('index.html'))
+	resp.set_cookie('id', id)
+	app.logger.debug("id: {}".format(id))
+	return resp
 
 @app.route("/loading", methods=['GET','POST'])
 def loading():
-    id = request.cookies.get('id')
-    songName1 = ''
-    songName2 = ''
-    if request.method == 'POST':
+	id = request.cookies.get('id')
+	songName1 = ''
+	songName2 = ''
+	if request.method == 'POST':
 		f = request.files['song1']
 		songName1 = request.form['songName1']
 		f.save('static/song1' + id + '.mp3')
 		f = request.files['song2']
 		songName2 = request.form['songName2']
 		f.save('static/song2' + id + '.mp3')
-    return render_template('loading.html')
+	return render_template('loading.html')
 
 @socketio.on('connect', namespace='/song_done')
 def mash():
-    @copy_current_request_context
-    def load_song():
-        id = request.cookies.get('id')
-        mix_from_paths('static/song1' + id + '.mp3','static/song2' + id + '.mp3', 'thisisntevenmyfinalform' + id + '.mp3')
-        emit('song done', {'data': 'the song has been created'})
-    thread = Thread(target = load_song, args=())
-    thread.start()
+	@copy_current_request_context
+	def load_song():
+		id = request.cookies.get('id')
+		mix_from_paths('static/song1' + id + '.mp3','static/song2' + id + '.mp3', 'thisisntevenmyfinalform' + id + '.mp3')
+		emit('song done', {'data': 'the song has been created'})
+	thread = Thread(target = load_song, args=())
+	thread.start()
 
 @app.route("/download")
 def download():
-    userId = request.cookies.get('id')
-    return render_template('download.html', id=userId)
+	userId = request.cookies.get('id')
+	return render_template('download.html', id=userId)
 
 @app.route("/info")
 def info():
-    id = request.cookies.get('id')
-    strings = getVerbal(getFeedback(getSongInfo('static/song1' + id + '.mp3'), getSongInfo('static/song2') + id + '.mp3'))
-    strings = "According to MusicGraph, " + strings
-    return strings
+	id = request.cookies.get('id')
+	strings = getVerbal(getFeedback(getSongInfo('static/song1' + id + '.mp3'), getSongInfo('static/song2') + id + '.mp3'))
+	strings = "According to MusicGraph, " + strings
+	return strings
 
 if __name__ == "__main__":
-    print "running on port 80"
-    socketio.run(app, host='0.0.0.0', port=80)
+	host = '0.0.0.0'
+	port = 8000
+	app.debug = True
+	level = logging.DEBUG
+	
+#	logger = logging.getLogger(__name__)
+	file_handler = logging.FileHandler('log', mode='w')
+	file_handler.setLevel(level)
+#	logger.addHandler(file_handler)
+	app.logger.addHandler(file_handler)
+	logging.getLogger("werkzeug").addHandler(file_handler)
+	
+	app.logger.info("started running with host {} on port {}, debug = {}".format(host, port, app.debug))
+	socketio.run(app, host=host, port=port)
